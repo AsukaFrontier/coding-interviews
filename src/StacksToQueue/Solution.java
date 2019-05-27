800ba55b1157ae2f33589ddddef616d463ab3534
AsukaFrontier <linkin9411@icloud.com>
2019-05-27 16:57:29 +0800

    Revise StacksToQueue;

--- src/StacksToQueue/Solution.java
+++ src/StacksToQueue/Solution.java
@@ -6,10 +6,39 @@ public class Solution {
 
     public void push(int node)
     {
-        //
+        //enqueue
+        stack1.push(node);
     }
     public int pop()
     {
+        //dequeue
+        stack2queue();
+        return stack2.pop();
+    }
+    private void stack2queue()
+    {
         //
+        while(stack2.isEmpty())
+        {
+            while(!stack1.isEmpty())
+            {
+                stack2.push(stack1.pop());
+            }
+        }
+    }
+    //-------UNIT TEST----------------//
+    public static void main(String[] args)
+    {
+        Solution s= new Solution();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        s.push(4);
+        System.out.println(s.pop());
+        s.push(5);
+        System.out.println(s.pop());
+        System.out.println(s.pop());
     }
 }
