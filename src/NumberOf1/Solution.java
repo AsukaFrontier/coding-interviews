f209ddf8afa002c80da7776f766396c797f344d9
AsukaFrontier <linkin9411@icloud.com>
2019-05-27 18:55:42 +0800

    Add RectCover;

---
+++ src/NumberOf1/Solution.java
@@ -0,0 +1,8 @@
+package NumberOf1;
+
+public class Solution {
+    public int numberOf1(int n)
+    {
+        //
+    }
+}
---
+++ src/RectCover/Solution.java
@@ -0,0 +1,9 @@
+package RectCover;
+
+public class Solution {
+    public int RectCover(int target)
+    {
+        //
+        return 0;
+    }
+}
