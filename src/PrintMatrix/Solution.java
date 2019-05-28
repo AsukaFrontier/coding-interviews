13e0f409cc4977b289587c30ff40387e0c9c3687
AsukaFrontier <linkin9411@icloud.com>
2019-05-28 22:54:42 +0800

    Add PrintMatrix;

--- src/PrintMatrix/Solution.java
+++ src/PrintMatrix/Solution.java
@@ -5,7 +5,24 @@ import java.util.ArrayList;
 public class Solution {
     public ArrayList<Integer> printMatrix(int[][] matrix)
     {
+        //Special circumstance;
+        if(matrix.length==0)
+            return null;
+        if(matrix[0].length==0)
+            return null;
+        //
         ArrayList<Integer> ints= new ArrayList<>();
         return ints;
+    }
+    private void printBlockMatrix(int[][] matrix)
+    {
+        //
+        int row=matrix.length;
+        int col=matrix[0].length;
+    }
+    //----------------UNIT TEST---------------//
+    public static void main(String[] args)
+    {
+        int[][] matrix={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
     }
 }
