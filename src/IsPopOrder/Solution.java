483d9ada90a1953e6ee1ecb7a9fbccb1d056bd7f
AsukaFrontier <linkin9411@icloud.com>
2019-05-29 11:46:41 +0800

    Add IsPopOrder;

---
+++ src/IsPopOrder/Solution.java
@@ -0,0 +1,12 @@
+package IsPopOrder;
+import java.util.Stack;
+public class Solution {
+    public boolean isPopOrder(int[] pushA, int[] popA)
+    {
+        //
+    }
+    private Stack<Integer> arrayPush(int[] pushA)
+    {
+        //
+    }
+}
