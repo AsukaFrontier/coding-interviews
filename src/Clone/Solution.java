017a125f48c004c881e20c8dcb174fe21b0e566e
AsukaFrontier <linkin9411@icloud.com>
2019-06-12 14:23:53 +0800

    add Clone; revise ReadMe;

---
+++ src/Clone/Solution.java
@@ -0,0 +1,18 @@
+package Clone;
+
+public class Solution {
+    public RandomListNode clone(RandomListNode pHead)
+    {
+        //
+    }
+    private class RandomListNode
+    {
+        int lable;
+        RandomListNode next;
+        RandomListNode random;
+        RandomListNode(int label)
+        {
+            this.lable=label;
+        }
+    }
+}
