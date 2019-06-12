41a0cb9665c713ccaa3cf008f5d237e0347fcc79
AsukaFrontier <linkin9411@icloud.com>
2019-06-12 21:25:53 +0800

    add PrintMinNumber;

---
+++ src/PrintMinNumber/Solution.java
@@ -0,0 +1,28 @@
+package PrintMinNumber;
+import java.util.ArrayList;
+public class Solution {
+    //
+    public String printMinNumber(int[] numbers)
+    {
+        //
+    }
+    private ArrayList<Integer> permutationToString(int[] array)
+    {
+        //
+        ArrayList<Integer> aList= new ArrayList<Integer>();
+        int array_length= array.length;
+        boolean[] visited= new boolean[array_length];
+        for(int i=0;i<array_length;i++)//length of permutation
+        {
+            for(int j=0;j<array_length-i;j++)
+            {
+                //
+            }
+        }
+    }
+    //-----------UNIT TEST-----//
+    public static void main(String[] args)
+    {
+        //
+    }
+}
