bd93b16ce1e5c659cc0931db58dba90115eaa037
AsukaFrontier <linkin9411@icloud.com>
2019-06-24 14:12:47 +0800

    revise SearchIn2DimensionalArray; revise ReverseArrayList;

--- src/SearchIn2DimensionalArray/Solution.java
+++ src/SearchIn2DimensionalArray/Solution.java
@@ -8,15 +8,4 @@ public class Solution {
                     return true;
         return false;
     }
-    //------------------UNIT TEST---------------------------------//
-    public static void main(String[] args)
-    {
-        Solution s= new Solution();
-        int[] [] array_2D={{1,2,3,4},{2,3,4,5},{3,4,5,6}};
-        int target=5;
-        if(s.find(target,array_2D))
-            System.out.println("Contains.");
-        else
-            System.out.println("Not contains.");
-    }
 }
