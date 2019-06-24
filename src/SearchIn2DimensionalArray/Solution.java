9b5fd8ca4e184254077b1b1cbfa1c74cb301133e
AsukaFrontier <linkin9411@icloud.com>
2019-06-24 14:08:42 +0800

    revise SearchIn2DimensionalArray;

--- src/SearchIn2DimensionalArray/Solution.java
+++ src/SearchIn2DimensionalArray/Solution.java
@@ -1,23 +1,20 @@
 package SearchIn2DimensionalArray;
-
 public class Solution {
-    public boolean Find(int target, int [][] array)
+    public boolean find(int target, int [][] array)
     {
-        //
         for(int i=0;i<array.length;i++)
             for(int j=0;j<array[0].length;j++)
                 if(array[i][j]==target)
                     return true;
         return false;
     }
-    //---------------------------------------------------//
+    //------------------UNIT TEST---------------------------------//
     public static void main(String[] args)
     {
-        //
+        Solution s= new Solution();
         int[] [] array_2D={{1,2,3,4},{2,3,4,5},{3,4,5,6}};
         int target=5;
-        Solution s= new Solution();
-        if(s.Find(target,array_2D))
+        if(s.find(target,array_2D))
             System.out.println("Contains.");
         else
             System.out.println("Not contains.");
