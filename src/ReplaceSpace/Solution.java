0d76bf1a502b2d77fdc28b0418f7f1b16391125d
AsukaFrontier <linkin9411@icloud.com>
2019-06-24 14:11:10 +0800

    revise ReplaceSpace;

--- src/ReplaceSpace/Solution.java
+++ src/ReplaceSpace/Solution.java
@@ -1,16 +1,7 @@
 package ReplaceSpace;
-
 public class Solution {
     public String replaceSpace(StringBuffer str)
     {
-        //
-        String s=str.toString();
-        s=s.replaceAll(" ","%20");
-        return s;
-    }
-    //---------------------------------------------/
-    public static void main(String[] args)
-    {
-        //
+        return str.toString().replaceAll(" ","%20");
     }
 }
