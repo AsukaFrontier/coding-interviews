30c96fde26f971c900414e25c41674aacd4f9ec9
AsukaFrontier <linkin9411@icloud.com>
2019-09-03 20:32:53 +0800

    revise;

--- src/FindPath/SolutionRevise.java
+++ src/FindPath/SolutionRevise.java
@@ -2,8 +2,23 @@ package FindPath;
 import java.util.ArrayList;
 public class SolutionRevise //
 {
-    //
+    ArrayList<ArrayList<Integer>> allPaths= new ArrayList<>();
+    ArrayList<ArrayList<Integer>> selectedPaths= new ArrayList<>();
+    ArrayList<ArrayList<Integer>> sortedPaths= new ArrayList<>();
     public ArrayList<ArrayList<Integer>> findPath(TreeNode root, int target)
+    {
+        if(root==null)
+            return sortedPaths;
+    }
+    private ArrayList<ArrayList<Integer>> findAllPaths(TreeNode root)
+    {
+        //
+    }
+    private ArrayList<ArrayList<Integer>> selectPaths(ArrayList<ArrayList<Integer>> allPaths)
+    {
+        //
+    }
+    private ArrayList<ArrayList<Integer>> sortPaths(ArrayList<ArrayList<Integer>> selectedPaths)
     {
         //
     }
