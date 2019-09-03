82427081f9650dc2283f59273251f35a8ce9b9db
AsukaFrontier <linkin9411@outlook.com>
2019-09-03 23:16:26 +0800

    revise;

--- src/FindPath/ProtoType.java
+++ src/FindPath/ProtoType.java
@@ -2,10 +2,14 @@ package FindPath;
 import java.util.ArrayList;
 public class ProtoType //
 {
-    ArrayList<Integer> aListInts= new ArrayList<>();
-    public void depthFirstVisit(TreeNode root)
+    private ArrayList<Integer> aListInts= new ArrayList<>();
+    public void depthFirstVisit(TreeNode root) //深度优先遍历
     {
-        //
+        if(root==null)
+            return ;
+        aListInts.add(root.val);
+        depthFirstVisit(root.left);
+        depthFirstVisit(root.right);
     }
     private class TreeNode
     {
@@ -16,5 +20,24 @@ public class ProtoType //
         {
             this.val= val;
         }
+    }
+    //--------------UNIT TEST---------------//
+    public static void main(String[] args)
+    {
+        ProtoType s= new ProtoType();
+        TreeNode tn0=s.new TreeNode(0);
+        TreeNode tn1=s.new TreeNode(1);
+        TreeNode tn2=s.new TreeNode(2);
+        TreeNode tn3=s.new TreeNode(6);
+        TreeNode tn4=s.new TreeNode(3);
+        TreeNode tn5=s.new TreeNode(3);
+        tn0.left=tn1;
+        tn0.right=tn2;
+        tn2.right=tn3;
+        tn3.left=tn4;
+        tn3.right=tn5;
+        s.depthFirstVisit(tn0);
+        for(int i=0;i<s.aListInts.size();i++)
+            System.out.print(s.aListInts.get(i)+" ");
     }
 }
--- src/FindPath/SolutionRevise.java
+++ src/FindPath/SolutionRevise.java
@@ -1,9 +1,27 @@
 package FindPath;
 import java.util.ArrayList;
+import java.util.LinkedList;
 public class SolutionRevise //
 {
-    //
+    private ArrayList<ArrayList<Integer>> allPaths= new ArrayList<>();
+    private ArrayList<ArrayList<Integer>> selectedPaths= new ArrayList<>();
+    private ArrayList<ArrayList<Integer>> sortedPaths= new ArrayList<>();
     public ArrayList<ArrayList<Integer>> findPath(TreeNode root, int target)
+    {
+        if(root==null)
+            return sortedPaths;
+    }
+    private void findAllPaths(TreeNode root)
+    {
+        //
+        LinkedList<Integer> queue= new LinkedList<>(); //Queue: add(), remove();
+        queue.add(root.val);
+        if(root.left==null&&root.right==null) //当前节点是叶子节点
+        {
+            allPaths.add(queueToArrayList(queue));
+        }
+    }
+    private ArrayList<Integer> queueToArrayList(LinkedList<Integer> queue)
     {
         //
     }
@@ -17,4 +35,12 @@ public class SolutionRevise //
             this.val= val;
         }
     }
+    //----------UNIT TEST----------//
+    //          3
+    //         / \
+    //        1   2
+    //           / \
+    //          3   4
+    //         /
+    //        1
 }
