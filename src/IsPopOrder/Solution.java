e3030b176789cf07b50cb9c44815dde54bea3048
AsukaFrontier <linkin9411@icloud.com>
2019-09-03 16:45:18 +0800

    revise;

--- src/IsPopOrder/Solution.java
+++ src/IsPopOrder/Solution.java
@@ -1,28 +1,32 @@
 package IsPopOrder;
-import java.util.Stack;
-public class Solution//
+import java.util.LinkedList;
+public class Solution //
 {
     public boolean isPopOrder(int[] pushA, int[] popA)
     {
-        Stack<Integer> stack = new Stack<>();
+        LinkedList<Integer> auxilliaryStack = new LinkedList<>(); //辅助栈
+        //迭代数组popA, 按照入栈顺序将pushA中元素压入辅助栈中(包括popA对应的元素)
+        //出栈数组popA中的某元素对应着入栈数组pushA的元素, 其(pushA中元素)左边的所有元素都应已入栈
         int index_pushA=0;
         int index_popA=0;
-        while(index_popA<popA.length)
+        while(index_popA<popA.length) //迭代数组popA
         {
-            //
-            while(stack.isEmpty()||stack.peek()!=popA[index_popA])
+            //对于一个正确的入栈出栈顺序, 若辅助栈栈首元素与popA数组当前元素不符时, 应当继续入栈, 直到相符(这是合理出栈顺序所对应的逻辑)
+            while(auxilliaryStack.isEmpty()||auxilliaryStack.peek()!=popA[index_popA])
             {
-                if(index_pushA>=pushA.length)
+                if(index_pushA>=pushA.length) //即已经按照pushA数组提供的顺序将元素都压入栈中, 但仍不符合
                     return false;
-                stack.push(pushA[index_pushA]);
+                auxilliaryStack.push(pushA[index_pushA]);
                 index_pushA++;
             }
-            stack.pop();
+            //当辅助栈中的栈首元素与当前popA[index_popA]相同时, 意味着该元素需要出栈操作(才能在popA数组中出现)
+            auxilliaryStack.pop();
             index_popA++;
         }
         return true;
     }
-    //----------UNIT TEST------------//
+    /**
+    //--------------UNIT TEST----------------//
     public static void main(String[] args)
     {
         //
@@ -34,4 +38,5 @@ public class Solution//
         else
             System.out.println("False.");
     }
+     */
 }
